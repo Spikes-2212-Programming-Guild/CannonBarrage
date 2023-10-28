259b12f0a5babce4c57b1f897f4f480ba8a6b84d
TurtleMinecraft <schnitzelgirl69@gmail.com>
2023-10-28 15:08:45 +0300

    Drivetrain extends Subsystem

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -1,8 +1,10 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj2.command.Subsystem;
+
 import java.util.function.Supplier;
 
-public interface Drivetrain {
+public interface Drivetrain extends Subsystem {
 
     void drive(Supplier<Double> xSpeed, Supplier<Double> ySpeed, Supplier<Double> rotationSpeed,
                boolean fieldRelative, boolean usePID);

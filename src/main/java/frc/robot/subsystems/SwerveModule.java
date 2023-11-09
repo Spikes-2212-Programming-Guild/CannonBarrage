b00659a2e5b8239e47e1431ad50dcd844fb82f91
TurtleMinecraft <schnitzelgirl69@gmail.com>
2023-11-09 17:08:45 +0200

    deleted SwerveModule

--- src/main/java/frc/robot/subsystems/SwerveModule.java
+++
@@ -1,8 +0,0 @@
-package frc.robot.subsystems;
-
-import edu.wpi.first.math.kinematics.SwerveModuleState;
-
-public interface SwerveModule {
-
-    void set(SwerveModuleState state, boolean useFeedForward);
-}

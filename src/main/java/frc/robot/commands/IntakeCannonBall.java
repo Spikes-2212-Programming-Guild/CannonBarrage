237ee87ab3c3065681a58eff89f181e8615aaf4d
nettahalamish <bygneta1611@gmail.com>
2023-11-09 21:03:00 +0200

    created class and subsystems.

---
+++ src/main/java/frc/robot/commands/IntakeCannonBall.java
@@ -0,0 +1,13 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import frc.robot.subsystems.IntakeRoller;
+import frc.robot.subsystems.Roundabout;
+import frc.robot.subsystems.Transfer;
+
+public class IntakeCannonBall extends ParallelCommandGroup {
+
+    public IntakeCannonBall(IntakeRoller intakeRoller, Roundabout roundabout, Transfer transfer) {
+
+    }
+}
---
+++ src/main/java/frc/robot/subsystems/Transfer.java
@@ -0,0 +1,6 @@
+package frc.robot.subsystems;
+
+import com.spikes2212.command.genericsubsystem.smartmotorcontrollersubsystem.SmartMotorControllerSubsystem;
+
+public interface Transfer extends SmartMotorControllerSubsystem {
+}
